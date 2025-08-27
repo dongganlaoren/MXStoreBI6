0214c3fa69cbbea4569ceefb74e06a2745974dc5
dongganlaoren <32191681@qq.com>
2025-08-27 20:52:57 +0800

    merge heads 20250827

---
+++ migrations/versions/a9f18f426c1e_merge_heads_20250827.py
@@ -0,0 +1,24 @@
+"""merge heads 20250827
+
+Revision ID: a9f18f426c1e
+Revises: 64753ae35be1, e5d89116a8fc
+Create Date: 2025-08-27 20:17:01.484128
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'a9f18f426c1e'
+down_revision = ('64753ae35be1', 'e5d89116a8fc')
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
