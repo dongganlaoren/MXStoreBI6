ea2eaa0391b4665516285c3eaf4e1468e1176230
dongganlaoren <32191681@qq.com>
2025-08-31 10:13:39 +0800

    统一 Alembic env.py 配置

--- migrations/env.py
+++ migrations/env.py
@@ -1,88 +1,56 @@
 import logging
 from logging.config import fileConfig
 
+from alembic import context
 from flask import current_app
 
-from alembic import context
-
-# this is the Alembic Config object, which provides
-# access to the values within the .ini file in use.
+# Alembic 配置对象
 config = context.config
 
-# Interpret the config file for Python logging.
-# This line sets up loggers basically.
+# 设置日志
 fileConfig(config.config_file_name)
 logger = logging.getLogger('alembic.env')
 
+# 目标 metadata
+# 确保导入项目所有 models，这样 Alembic 才能检测表结构
+from app import models  # 你的模型模块路径
 
+target_metadata = models.db.metadata  # SQLAlchemy db 对象的 metadata
+
+
+# 线上/开发数据库引擎获取
 def get_engine():
     try:
-        # this works with Flask-SQLAlchemy<3 and Alchemical
         return current_app.extensions['migrate'].db.get_engine()
     except (TypeError, AttributeError):
-        # this works with Flask-SQLAlchemy>=3
         return current_app.extensions['migrate'].db.engine
 
 
+# 数据库 URL 配置
 def get_engine_url():
     try:
-        return get_engine().url.render_as_string(hide_password=False).replace(
-            '%', '%%')
+        return str(get_engine().url).replace('%', '%%')
     except AttributeError:
         return str(get_engine().url).replace('%', '%%')
 
 
-# add your model's MetaData object here
-# for 'autogenerate' support
-# from myapp import mymodel
-# target_metadata = mymodel.Base.metadata
 config.set_main_option('sqlalchemy.url', get_engine_url())
-target_db = current_app.extensions['migrate'].db
-
-# other values from the config, defined by the needs of env.py,
-# can be acquired:
-# my_important_option = config.get_main_option("my_important_option")
-# ... etc.
 
 
-def get_metadata():
-    if hasattr(target_db, 'metadatas'):
-        return target_db.metadatas[None]
-    return target_db.metadata
-
-
+# Offline 模式
 def run_migrations_offline():
-    """Run migrations in 'offline' mode.
-
-    This configures the context with just a URL
-    and not an Engine, though an Engine is acceptable
-    here as well.  By skipping the Engine creation
-    we don't even need a DBAPI to be available.
-
-    Calls to context.execute() here emit the given string to the
-    script output.
-
-    """
     url = config.get_main_option("sqlalchemy.url")
     context.configure(
-        url=url, target_metadata=get_metadata(), literal_binds=True
+        url=url,
+        target_metadata=target_metadata,
+        literal_binds=True,
     )
-
     with context.begin_transaction():
         context.run_migrations()
 
 
+# Online 模式
 def run_migrations_online():
-    """Run migrations in 'online' mode.
-
-    In this scenario we need to create an Engine
-    and associate a connection with the context.
-
-    """
-
-    # this callback is used to prevent an auto-migration from being generated
-    # when there are no changes to the schema
-    # reference: http://alembic.zzzcomputing.com/en/latest/cookbook.html
     def process_revision_directives(context, revision, directives):
         if getattr(config.cmd_opts, 'autogenerate', False):
             script = directives[0]
@@ -95,18 +63,17 @@ def run_migrations_online():
         conf_args["process_revision_directives"] = process_revision_directives
 
     connectable = get_engine()
-
     with connectable.connect() as connection:
         context.configure(
             connection=connection,
-            target_metadata=get_metadata(),
+            target_metadata=target_metadata,
             **conf_args
         )
-
         with context.begin_transaction():
             context.run_migrations()
 
 
+# 根据模式执行
 if context.is_offline_mode():
     run_migrations_offline()
 else:
